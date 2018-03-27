5a8b4f4df8fa3d37057a9f24345afb08082ea92e
Guest User <Guest@wg401-g-04-06.mddn.vuw.ac.nz>
2018-03-27 16:49:07 +1300

    replaced files with ex2 site as it is already responsive

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-
-        // SCROLL REVEAL
-        window.sr = ScrollReveal({ reset: true });
-        //sr.reveal('.revealA, .revealB, .revealC, .revealD, .revealE', { origin: 'left',  duration: 1200, scale: 0.8});
-        sr.reveal('#revealA_1, #revealB_1, #revealC_1, #revealD_1, #revealD_2, #revealE_1', { origin: 'right',  duration: 1000 });
-
-    });
